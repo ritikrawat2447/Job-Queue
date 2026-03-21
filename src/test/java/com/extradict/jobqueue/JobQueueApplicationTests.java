7b98610a1ebd6a87bd90d8e55bc31c939bddd52c
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2026-03-22 01:36:30 +0530

    first commit

---
+++ src/test/java/com/extradict/jobqueue/JobQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extradict.jobqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
